0deb796d19931f26e3df87a1b905962f07776027
Mark Briscoe <mark@mcmw.co.uk>
2024-01-24 12:42:10 +0000

    added division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,27 +7,128 @@ document.addEventListener('DOMContentLoaded', function () {
   for (let button of buttons) {
     button.addEventListener('click', function () {
       if (this.getAttribute('data-type') === 'submit') {
-        alert('You clicked Submit!');
+        checkAnswer();
       } else {
         let gameType = this.getAttribute('data-type');
-        alert(`You clicked ${gameType}`);
+        runGame(gameType);
       }
     });
   }
+
+  document.getElementById('answer-box').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      checkAnswer();
+    }
+  });
+
+  runGame('addition');
 });
 
-function runGame() {}
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
+  document.getElementById('answer-box').value = '';
+  document.getElementById('answer-box').focus();
 
-function checkAnswer() {}
+  // Creates two random numbers between 1 and 25
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
 
-function calculateCorrectAnswer() {}
+  if (gameType === 'addition') {
+    displayAdditionQuestion(num1, num2);
+  } else if (gameType === 'multiply') {
+    displayMultiplyQuestion(num1, num2);
+  } else if (gameType === 'subtract') {
+    displaySubtractQuestion(num1, num2);
+  } else if (gameType === 'division') {
+    displayDivideQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
+  }
+}
 
-function incrementScore() {}
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
+function checkAnswer() {
+  let userAnswer = parseInt(document.getElementById('answer-box').value);
+  let calculatedAnswer = calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculatedAnswer[0];
 
-function incrementWrongAnswer() {}
+  if (isCorrect) {
+    alert('Hey! You got it right! :D');
+    incrementScore();
+  } else {
+    alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    incrementWrongAnswer();
+  }
 
-function displayAdditionQuestion() {}
+  runGame(calculatedAnswer[1]);
+}
 
-function displaySubtractQuestion() {}
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
+function calculateCorrectAnswer() {
+  let operand1 = parseInt(document.getElementById('operand1').innerText);
+  let operand2 = parseInt(document.getElementById('operand2').innerText);
+  let operator = document.getElementById('operator').innerText;
 
-function displayMultiplyQuestion() {}
+  if (operator === '+') {
+    return [operand1 + operand2, 'addition'];
+  } else if (operator === 'x') {
+    return [operand1 * operand2, 'multiply'];
+  } else if (operator === '-') {
+    return [operand1 - operand2, 'subtract'];
+  } else if (operator === '/') {
+    return [Math.floor(operand1 / operand2), 'division'];
+  } else {
+    alert(`Unimplemented operator ${operator}`);
+    throw `Unimplemented operator ${operator}. Aborting!`;
+  }
+}
+
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
+  let oldScore = parseInt(document.getElementById('score').innerText);
+  document.getElementById('score').innerText = ++oldScore;
+}
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+  let oldScore = parseInt(document.getElementById('incorrect').innerText);
+  document.getElementById('incorrect').innerText = ++oldScore;
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = '+';
+}
+
+function displaySubtractQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+  document.getElementById('operator').textContent = '-';
+}
+
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = 'x';
+}
+
+function displayDivideQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById('operand2').textContent = operand2 > operand1 ? operand1 : operand2;
+  document.getElementById('operator').textContent = '/';
+}
---
+++ assets/js/test.js
@@ -0,0 +1,12 @@
+let book = {
+  title: 'JavaScript Basics',
+  author: {
+    name: 'Jane Doe',
+    DOB: '12/12/2001',
+  },
+  pages: 200,
+  publisher: {
+    name: 'CodePress',
+    year: 2022,
+  },
+};
